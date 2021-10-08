2b69992167dad9d5e5a4bd3d117fb8e3575ead5b
Danny van der Weijde <dannyvdweijde@gmail.com>
2021-10-08 15:52:48 +0200

    Home page van stock website verder gemaakt.

--- Company-stock-website/script/index.js
+++ Company-stock-website/script/index.js
@@ -1,14 +1,17 @@
 var loading = document.getElementsByClassName("loading")[0];
 var title = loading.children[0].children[0];
-var vid = document.getElementsByClassName("header__video")[0];
 var nav = document.getElementsByClassName("nav")[0];
+var header__content_container = document.getElementsByClassName("header__content-container")[0];
 
-vid.playbackRate = 0.5;
+nav.classList.remove("nav--loose");
+
 document.onreadystatechange = function() 
 {
     var state = document.readyState;
     loading.style.display = "block";
     nav.classList.remove("nav--animation");
+    header__content_container.classList.remove("header__content-container--animation");
+
     if (state === "complete") {
         loading.style.transition = "1s ease-in";
         title.style.transition = "0.5s ease-in";
@@ -32,5 +35,18 @@ document.onreadystatechange = function()
         setTimeout(() => {  
             nav.classList.add("nav--animation");
         }, 2100);
+
+        setTimeout(() => {  
+            header__content_container.classList.add("header__content-container--animation");
+        }, 2500);
+    }
+}
+
+window.onscroll = function()
+{
+    if (window.pageYOffset === 0) {
+        nav.classList.remove("nav--loose");
+    } else if (!nav.classList.contains("nav--loose")) {
+        nav.classList.add("nav--loose");
     }
 }
