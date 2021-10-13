fe83a3ed90c9a3e484fc79c4e3de3c76f4f49d9d
Danny van der Weijde <dannyvdweijde@gmail.com>
2021-10-13 16:23:26 +0200

    Homepagina verder gemaakt.

--- Company-stock-website/script/index.js
+++ Company-stock-website/script/index.js
@@ -2,8 +2,23 @@ var loading = document.getElementsByClassName("loading")[0];
 var title = loading.children[0].children[0];
 var nav = document.getElementsByClassName("nav")[0];
 var header__content_container = document.getElementsByClassName("header__content-container")[0];
+var slideshow__inner = document.getElementsByClassName("slideshow__inner")[0];
+var transform = "0";
 
-nav.classList.remove("nav--loose");
+if (window.pageYOffset === 0) {
+    nav.classList.remove("nav--loose");
+} else if (!nav.classList.contains("nav--loose")) {
+    nav.classList.add("nav--loose");
+}
+
+window.onscroll = function()
+{
+    if (window.pageYOffset === 0) {
+        nav.classList.remove("nav--loose");
+    } else if (!nav.classList.contains("nav--loose")) {
+        nav.classList.add("nav--loose");
+    }
+}
 
 document.onreadystatechange = function() 
 {
@@ -15,6 +30,7 @@ document.onreadystatechange = function()
     if (state === "complete") {
         loading.style.transition = "1s ease-in";
         title.style.transition = "0.5s ease-in";
+        slideshow();
 
         setTimeout(() => {  
             title.style.top = "-50%";
@@ -42,11 +58,19 @@ document.onreadystatechange = function()
     }
 }
 
-window.onscroll = function()
-{
-    if (window.pageYOffset === 0) {
-        nav.classList.remove("nav--loose");
-    } else if (!nav.classList.contains("nav--loose")) {
-        nav.classList.add("nav--loose");
+function slideshow() {
+    transform--;
+    if (transform > -320) {
+        slideshow__inner.style.transform = "translateX(" + transform + "%)";
+    } else {
+        slideshow__inner.style.transition = "none 0s ease 0s";
+        transform = 0;
+        slideshow__inner.style.transform = "translateX(" + transform + "%)";
+        setTimeout(() => {  
+            slideshow__inner.style.transition = "0.5s linear transform";
+        }, 100);
     }
+    setTimeout(() => {  
+        slideshow();
+    }, 400);
 }
